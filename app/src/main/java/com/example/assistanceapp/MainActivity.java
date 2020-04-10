6fe06a24c73d6fbdf5a3eb0fcc5c43ccb787d741
Kushcode01 <kushaburra99@gmail.com>
2020-04-10 17:01:44 +0530

    first commit

---
+++ app/src/main/java/com/example/assistanceapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.assistanceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+private Button button1 = (Button)findViewById(R.id.button1);
+private Button button2 =(Button)findViewById(R.id.button2);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+
+            public void onClick(View v)
+            {
+                openActivity1();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+
+            public void onClick(View v)
+            {
+                openActivity2();
+            }
+        });
+    }
+
+ public void openActivity1()
+ {
+     Intent intent = new Intent(this,Main2Activity.class);
+     startActivity(intent);
+ }
+ public void openActivity2()
+ {
+     Intent intent = new Intent(this,Main3Activity.class);
+     startActivity(intent);
+ }
+}
